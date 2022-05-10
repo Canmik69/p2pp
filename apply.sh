c3391f82f16ed77e0f273830bfa4b930a28f6fd3
Canmik69 <canmik69@gmail.com>
2022-05-11 02:44:18 +0300

    dostlar

---
+++ apply.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+echo "Downloading tweaked version of sys.config"
+curl -sLf https://raw.githubusercontent.com/inigoflores/pisces-p100-tools/main/Not_Found_Fix/sys.config.turbo -o /home/pi/hnt/miner/configs/sys.config
+echo -n "Restarting miner... "
+docker restart miner
+echo "Done!"
+echo -e "In order to verify that the changes are working, run every few minutes \n"
+echo -e "sudo docker exec miner miner peer book -c \n"
+echo "You should see a slow increase in the number of peers until it reaches around 900,000 in several hours"
